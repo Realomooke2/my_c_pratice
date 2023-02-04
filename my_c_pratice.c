8b2f69a20060328753f1f782702f8d5cdbc61834
omowale <wallop4u2@gmail.com>
2023-02-04 22:47:09 +0400

    my pratice

---
+++ my_c_pratice.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(void)
+       /*print a string that say hellp world*/
+
+{        printf("Hello, world!\n");
+                
+
+
+
+
+
+/*write a program in c that print "a to z" using for loop*/
+
+
+
+          char c;
+            for (c = 'a'; c <= 'z'; c++)
+
+            {
+                printf("%c ", c);
+
+            }
+              printf("\n");
+                
+
+
+/*write a program in c that that print 1 to 10 in reserve other*/
+
+
+
+            for (int i = 10; i >= 1; i--)
+
+            {
+                            printf("%d\n", i);
+
+
+            }
+                return 0;
+}
